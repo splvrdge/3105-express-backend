b857502e28921554f19e054c0e0af92359704ef3
Francis James Lagang <francisjameslagang@Franciss-MacBook-Air.local>
2024-10-06 18:21:15 +0800

    generated secretKey and added authMiddleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const secretKey = process.env.SECRET_KEY;
+
+function authToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = authToken;
