354c1a1a9f98fd91754ecf0ca846a3ec2b085813
Francis James Lagang <francisjameslagang@Franciss-MacBook-Air.local>
2024-10-06 19:08:34 +0800

    added rate limiter

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const logger = require("./middleware/loggingMiddleware");
 const routes = require("./routes/user");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 30 * 1000,
+  max: 5,
+  message: "Too many requests! Try again after 30 seconds.",
+});
 
 const app = express();
 
+app.use(limiter);
 app.use(express.json());
 app.use(logger);
 app.use("/api/user", routes);
